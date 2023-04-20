e992e70c1dd21a9a16df909c6a9116e833f35798
HannaW1986 <131193450+HannaW1986@users.noreply.github.com>
2023-04-20 20:03:39 +0200

    next update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+       /*
         String[] cities = new String[5];
         cities[0]="Warsaw";
         cities[1]="Rome";
@@ -20,17 +21,53 @@ public class App
             String city = cities[i];
             System.out.println(city);}
 
-//        System.out.println("Zadanie 2,podpunkt drugi");
-//        for (Integer i=cities.length-1; i<cities.length; i--){
-//            String city1 = cities[i];
-//            System.out.println(city1);}
+        System.out.println("Zadanie 2,podpunkt drugi");
+        for (Integer i=cities.length-1; i>=0; i--){
+            String city1 = cities[i];
+            System.out.println(city1);}
 
         System.out.println("Zadanie 2,podpunkt trzeci");
         for (Integer i=0; i<cities.length; i+=2){
             String city2 = cities[i];
             System.out.println(city2);}
 
+        */
+
+        System.out.println("Zadanie 3");
+
+        Float[] number = new Float[4];
+        number[0] =1.5f;
+        number[1] =2.5f;
+        number[2] =3.5f;
+        number[3] =4.5f;
+
+        System.out.println("SUM of all numbers in table");
+
+        Float suma = 0f;
+
+        for (Float i=0f; i< number.length; i++){
+            suma = suma + number[i];
+
         }
+        System.out.println(suma);
+
+        Float average = suma/number.length;
+
+        System.out.println(average);
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
 
 
     }
